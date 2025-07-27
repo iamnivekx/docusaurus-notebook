7aef601609dea078057d7e47e61be84984e4fdd0
peng-huang-ch <h499871809@gmail.com>
2025-07-28 00:18:00 +0800

    Add GitHub Actions workflow for deployment and update configuration
    - Introduced a new GitHub Actions workflow for deploying the website to GitHub Pages.
    - Added a `.nvmrc` file to specify Node.js version.
    - Updated Docusaurus configuration with the correct site URL and project details for deployment.

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -18,15 +18,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://iamnivekx.github.com/docusaurus-notebook',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'iamnivekx', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-notebook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
